8c6c39329ace25ded148f05c12692696f8fabd18
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-13 18:59:32 +0530

    Load pending loan applications

--- frontend/src/app/module/sales-executive/pending-application/pending-application.component.ts
+++ frontend/src/app/module/sales-executive/pending-application/pending-application.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomerDetails } from '../../../model/customer-details';
+import { CommonService } from '../../../service/common.service';
 
 @Component({
   selector: 'app-pending-application',
   templateUrl: './pending-application.component.html',
   styleUrl: './pending-application.component.css'
 })
-export class PendingApplicationComponent {
+export class PendingApplicationComponent implements OnInit {
 
+  pendingApplications: CustomerDetails[] = [];
+
+  constructor(private commonservice: CommonService) { }
+
+  ngOnInit(): void {
+    this.commonservice.getApplicationData().subscribe(data => {
+      this.pendingApplications = data.filter(c => c.loanStatus === 'Pending');
+    });
+  }
 }
