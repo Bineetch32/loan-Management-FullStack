10f1792c0f07baa1cf7e6f1661e81712bb228ff8
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-13 18:48:24 +0530

    Submit loan application directly from document page

--- frontend/src/app/module/sales-executive/loan-request-application/loan-request-application.component.ts
+++ frontend/src/app/module/sales-executive/loan-request-application/loan-request-application.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { CustomerDetails } from '../../../model/customer-details';
 import { CommonService } from '../../../service/common.service';
 
@@ -8,18 +9,14 @@ import { CommonService } from '../../../service/common.service';
   templateUrl: './loan-request-application.component.html',
   styleUrl: './loan-request-application.component.css'
 })
-
-
-
 export class LoanRequestApplicationComponent implements OnInit {
-
   cdd: CustomerDetails[];
   basic: boolean = true;
   upload: boolean = false;
-  confirm: boolean = false;
+  agreeTerms: boolean = false;
+  submitting: boolean = false;
 
   basicdetailsave: any;
-  
   basicdetails: FormGroup;
   documentUpload: FormGroup;
   selectedPanCopy: any;
@@ -29,261 +26,92 @@ export class LoanRequestApplicationComponent implements OnInit {
   selectedSignature: any;
   selectedCancelledCheque: any;
   selectedSalarySlip: any;
-  selectedsanctionLetter:any;
-  // reader=new FileReader();
-  imageSrc1:any;
-  imageSrc2:any;
-  imageSrc3:any;
-  imageSrc4:any;
-  imageSrc5:any;
-  imageSrc6:any;
-  imageSrc7:any;
-  imageSrc8:any;
+  selectedsanctionLetter: any;
+  imageSrc1: any;
+  imageSrc2: any;
+  imageSrc3: any;
+  imageSrc4: any;
+  imageSrc5: any;
+  imageSrc6: any;
+  imageSrc7: any;
+  imageSrc8: any;
 
-  constructor(public fb: FormBuilder, public common: CommonService) {}
+  constructor(public fb: FormBuilder, public common: CommonService, private router: Router) {}
 
   ngOnInit(): void {
-    (this.basicdetails = this.fb.group({
-      customerName: [""],
-      customerMobileno: [""],
-      customerDOB: [""],
-      customerEmailId: [""],
-      customerPanNo: [""],
-      customerAadharNo: [""],
-      customerGender: [""],
-      customerIncome: [""],
-      // cibil: [""],
-      loanStatus:[""],
-      verificationn:[""],
-
-      customerlocalAddress: this.fb.group({
-        pincode: [""],
-        areaName: [""],
-        cityName: [""],
-        district: [""],
-        state: [""],
-      }),
-      customerPermanentAddress: this.fb.group({
-        pincode: [""],
-        areaName: [""],
-        cityName: [""],
-        district: [""],
-        state: [""],
-      }),
-
-      guarantorDetails: this.fb.group({
-        guarantorMobileNo: [""],
-        guarantorName: [""],
-        guarantorEmailId: [""],
-        guarantorAddress: [""],
-      }),
-
-      customerBankAccountDetails: this.fb.group({
-        accountNumber: [""],
-        ifscCode: [""],
-        bankName: [""],
-        address: [""],
-      }),
-    })),
-      (this.documentUpload = this.fb.group({
-        documentId: [""],
-      }));
+    this.basicdetails = this.fb.group({
+      customerName: [''],
+      customerMobileno: [''],
+      customerDOB: [''],
+      customerEmailId: [''],
+      customerPanNo: [''],
+      customerAadharNo: [''],
+      customerGender: [''],
+      customerIncome: [''],
+      loanStatus: [''],
+      verificationn: [''],
+      customerlocalAddress: this.fb.group({ pincode: [''], areaName: [''], cityName: [''], district: [''], state: [''] }),
+      customerPermanentAddress: this.fb.group({ pincode: [''], areaName: [''], cityName: [''], district: [''], state: [''] }),
+      guarantorDetails: this.fb.group({ guarantorMobileNo: [''], guarantorName: [''], guarantorEmailId: [''], guarantorAddress: [''] }),
+      customerBankAccountDetails: this.fb.group({ accountNumber: [''], ifscCode: [''], bankName: [''], address: [''] })
+    });
+    this.documentUpload = this.fb.group({ documentId: [''] });
   }
 
-
-  // onSelectedFile1(event: any) {
-  //   alert("file..Added");
-  //   this.selectedPanCopy = event.target.files[0];
-  //    this.reader.onload = e => this.imageSrc1 = this.reader.result;
-  // this.reader.readAsDataURL(this.selectedPanCopy);
-  // }
-
-  onSelectedFile1(event: any) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imageSrc1 = reader.result as string;
-    };
-this.selectedPanCopy = event.target.files[0];
-  reader.readAsDataURL(this.selectedPanCopy);
-  }
-
-  
-  // onSelectedFile2(event: any) {
-  //   alert("file..Added");
-  //   this.selectedUidCopy = event.target.files[0];
-  //    this.reader.onload = e => this.imageSrc2 = this.reader.result;
-  // this.reader.readAsDataURL(this.selectedUidCopy);
-  // }
-
-
-  onSelectedFile2(event: any) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imageSrc2 = reader.result as string;
-    };
-this.selectedUidCopy = event.target.files[0];
-  reader.readAsDataURL(this.selectedUidCopy);
-  }
-
-  // onSelectedFile3(event: any) {
-  //   alert("file..Added");
-  //   this.selectedBankPassbookCopy = event.target.files[0];
-  //    this.reader.onload = e => this.imageSrc3 = this.reader.result;
-  // this.reader.readAsDataURL(this.selectedBankPassbookCopy);
-  // }
-
-  onSelectedFile3(event: any) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imageSrc3 = reader.result as string;
-    };
-this.selectedBankPassbookCopy = event.target.files[0];
-  reader.readAsDataURL(this.selectedBankPassbookCopy);
-  }
-
-
-  // onSelectedFile4(event: any) {
-  //   alert("file..Added");
-  //   this.selectedPhoto = event.target.files[0];
-  //    this.reader.onload = e => this.imageSrc4 = this.reader.result;
-  // this.reader.readAsDataURL(this.selectedPhoto);
-  // }
-
-  onSelectedFile4(event: any) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imageSrc4 = reader.result as string;
-    };
-this.selectedPhoto = event.target.files[0];
-  reader.readAsDataURL(this.selectedPhoto);
-  }
-
-  // onSelectedFile5(event: any) {
-  //   alert("file..Added");
-  //   this.selectedSignature = event.target.files[0];
-  //    this.reader.onload = e => this.imageSrc5 = this.reader.result;
-  // this.reader.readAsDataURL(this.selectedSignature);
-  // }
-
-  onSelectedFile5(event: any) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imageSrc5 = reader.result as string;
-    };
-this.selectedSignature = event.target.files[0];
-  reader.readAsDataURL(this.selectedSignature);
-  }
-
-
-  // onSelectedFile6(event: any) {
-  //   alert("file..Added");
-  //   this.selectedCancelledCheque = event.target.files[0];
-  //    this.reader.onload = e => this.imageSrc6 = this.reader.result;
-  // this.reader.readAsDataURL(this.selectedCancelledCheque);
-  // }
-
-  onSelectedFile6(event: any) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imageSrc6 = reader.result as string;
-    };
-this.selectedCancelledCheque = event.target.files[0];
-  reader.readAsDataURL(this.selectedCancelledCheque);
-  }
-
-
-  // onSelectedFile7(event: any) {
-  //   alert("file..Added");
-  //   this.selectedSalarySlip = event.target.files[0];
-  //    this.reader.onload = e => this.imageSrc7 = this.reader.result;
-  // this.reader.readAsDataURL(this.selectedSalarySlip);
-  // }
-
-  onSelectedFile7(event: any) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imageSrc7 = reader.result as string;
-    };
-this.selectedSalarySlip = event.target.files[0];
-  reader.readAsDataURL(this.selectedSalarySlip);
-  }
-
-  // onSelectedFile8(event: any) {
-  //   alert("file..Added");
-  //   this.selectedsanctionLetter = event.target.files[0];
-  //    this.reader.onload = e => this.imageSrc8 = this.reader.result;
-  // this.reader.readAsDataURL(this.selectedsanctionLetter);
-  // }
-
-  onSelectedFile8(event: any) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imageSrc8 = reader.result as string;
-    };
-this.selectedsanctionLetter = event.target.files[0];
-  reader.readAsDataURL(this.selectedsanctionLetter);
-  }
-
-
+  onSelectedFile1(event: any) { this.selectedPanCopy = event.target.files[0]; }
+  onSelectedFile2(event: any) { this.selectedUidCopy = event.target.files[0]; }
+  onSelectedFile3(event: any) { this.selectedBankPassbookCopy = event.target.files[0]; }
+  onSelectedFile4(event: any) { this.selectedPhoto = event.target.files[0]; }
+  onSelectedFile5(event: any) { this.selectedSignature = event.target.files[0]; }
+  onSelectedFile6(event: any) { this.selectedCancelledCheque = event.target.files[0]; }
+  onSelectedFile7(event: any) { this.selectedSalarySlip = event.target.files[0]; }
+  onSelectedFile8(event: any) { this.selectedsanctionLetter = event.target.files[0]; }
 
   basics() {
-    alert("Customer Name=" + this.basicdetails.controls["customerName"].value);
     this.upload = true;
     this.basic = false;
-    console.log(this.basicdetails.controls["customerName"].value);
     this.basicdetailsave = this.basicdetails.controls;
-    console.log(this.basicdetailsave);
   }
-  
+
   submit() {
+    if (!this.agreeTerms) {
+      alert('Please agree to the terms and conditions.');
+      return;
+    }
+
     const requiredFiles = [
-      this.selectedPanCopy,
-      this.selectedUidCopy,
-      this.selectedBankPassbookCopy,
-      this.selectedPhoto,
-      this.selectedSignature,
-      this.selectedCancelledCheque,
-      this.selectedSalarySlip,
-      this.selectedsanctionLetter
+      this.selectedPanCopy, this.selectedUidCopy, this.selectedBankPassbookCopy,
+      this.selectedPhoto, this.selectedSignature, this.selectedCancelledCheque,
+      this.selectedSalarySlip, this.selectedsanctionLetter
     ];
 
     if (requiredFiles.some(file => !file)) {
-      alert("Please upload all required documents before submitting.");
+      alert('Please select all 8 documents.');
       return;
     }
-    const document1 = JSON.stringify(this.basicdetails.value);
+
     const uploadDocument = new FormData();
+    uploadDocument.append('panCopy', this.selectedPanCopy);
+    uploadDocument.append('uidCopy', this.selectedUidCopy);
+    uploadDocument.append('bankPassBookCopy', this.selectedBankPassbookCopy);
+    uploadDocument.append('photo', this.selectedPhoto);
+    uploadDocument.append('signature', this.selectedSignature);
+    uploadDocument.append('cancelledCheck', this.selectedCancelledCheque);
+    uploadDocument.append('salarySlips', this.selectedSalarySlip);
+    uploadDocument.append('sanctionLetter', this.selectedsanctionLetter);
+    uploadDocument.append('document1', JSON.stringify(this.basicdetails.value));
 
-    uploadDocument.append("panCopy", this.selectedPanCopy);
-    uploadDocument.append("uidCopy", this.selectedUidCopy);
-    uploadDocument.append("bankPassBookCopy", this.selectedBankPassbookCopy);
-    uploadDocument.append("photo", this.selectedPhoto);
-    uploadDocument.append("signature", this.selectedSignature);
-    uploadDocument.append("cancelledCheck", this.selectedCancelledCheque);
-    uploadDocument.append("salarySlips", this.selectedSalarySlip);
-    uploadDocument.append("sanctionLetter", this.selectedsanctionLetter);
-
-    uploadDocument.append("document1", document1);
-      this.common.postDocument(uploadDocument).subscribe({
-        next: () => {
-          alert("Data Saved Successfully");
-          this.confirm = true;
-          this.upload = false;
-          this.basic = false;
-        },
-        error: (error) => {
-          console.error("Loan application submission failed", error);
-          alert("Unable to save the application. Please try again.");
-        }
-      });
+    this.submitting = true;
+    this.common.postDocument(uploadDocument).subscribe({
+      next: () => {
+        this.submitting = false;
+        this.router.navigateByUrl('/salesexecutive/sd');
+      },
+      error: (error) => {
+        this.submitting = false;
+        console.error(error);
+        alert('Unable to save the application. Please try again.');
+      }
+    });
   }
-
-  uploadDoc() {
-    this.confirm = true;
-    this.upload = false;
-    this.basic = false;
-
-    // this.documentSave = this(value);
-    // this.basicdetailsave.push(this.documentSave);
-  }
-  }
+}
