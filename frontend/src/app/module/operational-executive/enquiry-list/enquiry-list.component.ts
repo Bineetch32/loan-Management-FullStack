77229817ee32a6344771b99610af83cc38670361
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-13 19:35:21 +0530

    Update CIBIL result without page refresh

--- frontend/src/app/module/operational-executive/enquiry-list/enquiry-list.component.ts
+++ frontend/src/app/module/operational-executive/enquiry-list/enquiry-list.component.ts
@@ -2,19 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { Enquiry } from '../../../model/enquiry';
 import { CommonService } from '../../../service/common.service';
 
-
 @Component({
   selector: 'app-enquiry-list',
   templateUrl: './enquiry-list.component.html',
   styleUrl: './enquiry-list.component.css'
 })
-export class EnquiryListComponent  implements OnInit{
+export class EnquiryListComponent implements OnInit {
 
-  enqlist: Enquiry[];
+  enqlist: Enquiry[] = [];
   mailStatus: string;
+  checkingCibilId: number = null;
 
   constructor(private common: CommonService) { }
+
   ngOnInit(): void {
+    this.loadEnquiries();
+  }
+
+  loadEnquiries() {
     this.common.getEnquiryall().subscribe({
       next: (list: Enquiry[]) => {
         this.enqlist = list;
@@ -24,16 +29,6 @@ export class EnquiryListComponent  implements OnInit{
       }
     });
   }
-  
-
-
-
-
-  // //sendMailForEnquiry
-  // mailsend(id:number){
-  //   this.common.sendMailForEnquiry(id).subscribe();
-  //   console.log("mail sent")
-  // }
 
   mailsend(id: number) {
     this.common.sendMailForEnquiry(id).subscribe({
@@ -45,12 +40,35 @@ export class EnquiryListComponent  implements OnInit{
       }
     });
   }
-  
 
-  //check cibil
-  checkcibilscore(enq:Enquiry)
-  {
-    console.table(enq);
-    this.common.getcibilscore(enq).subscribe();
-}
+  checkcibilscore(enq: Enquiry) {
+    if (this.checkingCibilId === enq.id) {
+      return;
+    }
+
+    this.checkingCibilId = enq.id;
+
+    this.common.getcibilscore(enq).subscribe({
+      next: () => {
+        this.common.getEnquiryDetailsById(enq.id).subscribe({
+          next: (updatedEnquiry: Enquiry) => {
+            const index = this.enqlist.findIndex(item => item.id === enq.id);
+            if (index !== -1) {
+              this.enqlist[index] = updatedEnquiry;
+            }
+            this.checkingCibilId = null;
+          },
+          error: (error) => {
+            console.error('Error getting updated enquiry:', error);
+            this.checkingCibilId = null;
+          }
+        });
+      },
+      error: (error) => {
+        console.error('Error checking CIBIL:', error);
+        this.checkingCibilId = null;
+        alert('Unable to check CIBIL. Please try again.');
+      }
+    });
+  }
 }
