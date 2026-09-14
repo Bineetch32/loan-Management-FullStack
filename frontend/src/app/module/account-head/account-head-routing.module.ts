2c31e8faf4f7f67f5f977746b9e183a56cc43244
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-14 12:57:09 +0530

    Make Account Head open directly on applicant modules

--- frontend/src/app/module/account-head/account-head-routing.module.ts
+++ frontend/src/app/module/account-head/account-head-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { VarifiedApplicantComponent } from './varified-applicant/varified-applicant.component';
 import { SanctionApplicantComponent } from './sanction-applicant/sanction-applicant.component';
 import { UploadSanctionLetterComponent } from './upload-sanction-letter/upload-sanction-letter.component';
 import { SanctionLetterComponent } from './sanction-letter/sanction-letter.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'ah', pathMatch: 'full' },
+  { path: '', redirectTo: 'ah/va', pathMatch: 'full' },
   {
     path: 'ah',
-    component: DashboardComponent,
     children: [
+      { path: '', redirectTo: 'va', pathMatch: 'full' },
       { path: 'va', component: VarifiedApplicantComponent },
       { path: 'sa', component: SanctionApplicantComponent },
       { path: 'sl', component: SanctionLetterComponent },
