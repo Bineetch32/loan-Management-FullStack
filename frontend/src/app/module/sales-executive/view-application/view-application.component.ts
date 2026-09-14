017bf0bcae71ade16b44699b5da92858084468d2
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-14 09:49:15 +0530

    Show one application full details with back

--- frontend/src/app/module/sales-executive/view-application/view-application.component.ts
+++ frontend/src/app/module/sales-executive/view-application/view-application.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { CustomerDetails } from '../../../model/customer-details';
 import { CommonService } from '../../../service/common.service';
 
@@ -8,22 +10,18 @@ import { CommonService } from '../../../service/common.service';
   styleUrl: './view-application.component.css'
 })
 export class ViewApplicationComponent implements OnInit {
+  customer: CustomerDetails;
 
+  constructor(private route: ActivatedRoute, private commonservice: CommonService, private location: Location) { }
 
-
-  constructor(private commonservice:CommonService) { }
-  retrievedDoc:CustomerDetails[];
-  
-  
-  selectedFile:File;
-  
   ngOnInit(): void {
-      this.commonservice.getApplicationData().subscribe(data=>{
-        this.retrievedDoc=data;
-        console.log(this.retrievedDoc);
-      })
-     
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.commonservice.getApplicationDetailsById(id).subscribe(data => {
+      this.customer = data;
+    });
+  }
 
-    }
-
+  getBack(): void {
+    this.location.back();
+  }
 }
